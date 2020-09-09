3b2b8e64f6014c6fc68231329548712418477ddd
pedrohsampaio <pedrohsampaioo@outlook.com>
2020-09-08 22:56:25 -0300

    config: theme

---
+++ lib/app/core/theme/app_colors.dart
@@ -0,0 +1,23 @@
+part of 'app_theme.dart';
+
+class AppColors {
+  static Color get anotherPurple => const Color(0xFF9871F5);
+  static Color get purple => const Color(0xFF8257E5);
+  static Color get littlePurple => const Color(0xFF774DD6);
+  static Color get purpleDark => const Color(0xFF6842C2);
+  static Color get green => const Color(0xFF04D361);
+  static Color get anotherGreen => const Color(0xFF24EF7F);
+  static Color get delete => const Color(0xFFE33D3D);
+  static Color get linesInWhite => const Color(0xFFE6E6F0);
+  static Color get hover => const Color(0xFFEBEBF5);
+  static Color get background => const Color(0xFFF0F0F7);
+  static Color get shapes01 => const Color(0xFFFFFFFF);
+  static Color get shapes02 => const Color(0xFFFAFAFC);
+  static Color get textTitles => const Color(0xFF32264D);
+  static Color get textBase => const Color(0xFF6A6180);
+  static Color get textComplement => const Color(0xFF9C98A6);
+  static Color get textInput => const Color(0xFFC1BCCC);
+  static Color get textInPurpleTitle => const Color(0xFFFFFFFF);
+  static Color get textInPurpleBase => const Color(0xFFD4C2FF);
+  static Color get textInPurpleComplement => const Color(0xFFA380F6);
+}
